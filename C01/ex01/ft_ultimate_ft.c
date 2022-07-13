d847290644e2273229ec02645bf2c2f279368e22
Fabrice FOURNIER <fabrice.fournier1210@gmail.com>
2022-07-13 13:30:47 +0200

    ex01

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void ft_ultimate_ft(int *********nbr)
+{
+    *********nbr = 42;
+}
+
+void	ft_ultimate_ft(int *********nbr);	
+
+int		main(void)
+{
+	int a, *b, **c, ***d, ****e, *****f, ******g, *******h, ********i, *********j;
+
+
+	j = &i;
+	i = &h;
+	h = &g;
+	g = &f;
+	f = &e;
+	e = &d;
+	d = &c;
+	c = &b;
+	b = &a;
+
+	ft_ultimate_ft(j);
+	if (a == 42)
+	{
+		write(1, "OK\n", 3);
+	}
+	else
+	{
+		write(1, "FAIL\n", 5);
+	}
+}
