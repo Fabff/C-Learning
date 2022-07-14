e4186923ce4693c50f7b7c0f60d48003740133a3
Fabrice FOURNIER <fabrice.fournier1210@gmail.com>
2022-07-14 11:29:55 +0200

    ex01

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+char *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i = 0;
+
+    while(n > i && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (n > i)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+    return dest;
+}
+
+int		main()
+{
+	int i;
+	char dest[] = "Epic fail !!!!!";
+	char src[] = "Success";
+	printf("%s\n%s\n%s\n", src, ft_strncpy(dest, src, 15), dest);
+	i = 9;
+	while (i < 15)
+	{
+		if (dest[i] != '\0')
+		{
+			printf("FAIL! Remainder of dst must be filled with `\\0' characters, got %c (\\%x) at pos %d\n", dest[i], dest[i], i);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
