d444b5a10d1ac78bed61d7432461de05b2cd11b6
Fabrice FOURNIER <fabrice.fournier1210@gmail.com>
2022-07-15 20:17:58 +0200

    ex08

---
+++ C02/ex08/ft_strlowcase.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+char *ft_strlowcase(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+        i++;
+    }
+    return str;
+}
+
+void	ft_putstr(char *str)
+{
+	int l;
+
+	l = 0;
+	while (str[l])
+	{
+		l++;
+	}
+	write(1, str, l);
+}
+
+int		main()
+{
+	char c[] = "AbCdEfG-Yz";
+	ft_putstr("abcdefg-yz:");
+	ft_putstr(ft_strlowcase(c));
+	ft_putstr(":");
+	ft_putstr(c);
+}
